b3f89ca7ad186b48e88e344b55c450c7955bdb3f
PauloHenriqueMagno <79842305+PauloHenriqueMagno@users.noreply.github.com>
2021-05-21 10:56:08 -0300

    Add files via upload

---
+++ introduction/modulo1.js
@@ -0,0 +1,11 @@
+var nome = "Brubs";
+var idade = 18;
+var idade2 = 10;
+
+//alert("Bem vindo " + nome);
+
+//alert(nome + " tem " + idade + " anos");
+
+//alert("Introduction to javascript");
+
+alert(idade + idade2)
